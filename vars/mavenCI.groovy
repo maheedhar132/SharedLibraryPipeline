b1ac251a74f49cd0485d81c1a4d1368159f042d0
--replace- <threkashri.p35@wipro.com>
2021-08-15 11:39:38 +0530

    groovys added

---
+++ vars/mavenCI.groovy
@@ -0,0 +1,32 @@
+def call(body){
+		def config = [:]
+		body.resloveStrategy = Closure.DELEGATE_FIRST
+		body.delegate = config
+		body()
+		def buildGoals = config.buildGoals
+		def pomFileLocation = config.buildFileLocation
+		def propertiesFileLocation = config.propertiesFileLocation
+			node{
+			Stage("buildType build"){
+			//Maven stage
+			maven{
+				mavenGoals = "${buildGoals}"
+				pomLocation = "${pomFileLocation}
+				}
+				}
+			//Sonar Stage
+			stage("Sonar Scan"){
+				sonar{
+				sonarPropertiesFile = "${propertiesFileLocation}"
+				}	
+			}
+			Stage("wait for Quality Gate"){
+				timeout (time: 1, unit: 'HOURS'){
+					def qualityGate = waitForQualityGate()
+					if(qualityGate.status != 'OK'){
+						error "Pipeline failed due to quality gate failure: ${qualityGate.status}"
+					}
+				}
+			}
+			}
+}
