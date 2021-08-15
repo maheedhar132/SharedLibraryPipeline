efe70da904bb74e96891ae9c9e943b7283a67e01
--replace- <threkashri.p35@wipro.com>
2021-08-15 11:54:30 +0530

    groovys added

--- vars/mavenCI.groovy
+++ vars/mavenCI.groovy
@@ -7,6 +7,9 @@ def call(body){
 		def pomFileLocation = config.buildFileLocation
 		def propertiesFileLocation = config.propertiesFileLocation
 			node{
+			stage("scm checkout"){
+			checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/maheedhar132/task.git']]])
+			}
 			stage("buildType build"){
 			//Maven stage
 			maven{
